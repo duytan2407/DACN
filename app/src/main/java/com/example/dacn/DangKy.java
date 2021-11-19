d487591dd91ef062d1c409e81bc98e449fd61e6b
Tân <nguyendam554@gmail.com>
2021-11-19 22:07:24 +0700

    đã fix

---
+++ app/src/main/java/com/example/dacn/DangKy.java
@@ -0,0 +1,17 @@
+package com.example.dacn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class DangKy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_dang_ky);
+    }
+}
