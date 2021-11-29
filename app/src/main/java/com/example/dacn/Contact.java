1fac035cef543c8d2d30b18d26add88eb6044e3d
Luanne <viluan922@gmail.com>
2021-11-29 16:41:00 +0700

    tụi m cho Luan lam do an rieng dung ko :))

--- app/src/main/java/com/example/dacn/Contact.java
+++ app/src/main/java/com/example/dacn/Contact.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ListBac extends AppCompatActivity {
+public class Contact extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_bac);
+        setContentView(R.layout.activity_contact);
     }
 }
