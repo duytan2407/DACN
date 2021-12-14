a821910f1d34b496d5eadd48f99c38c0bd25ac5c
Ny4b0r0n <Ny4b0r0n>
2021-12-15 02:57:32 +0700

    date/time

--- app/src/main/java/com/example/dacn/views/PaymentFragment.java
+++ app/src/main/java/com/example/dacn/views/PaymentFragment.java
@@ -3,13 +3,19 @@ package com.example.dacn.views;
 import static android.app.Activity.RESULT_CANCELED;
 import static android.app.Activity.RESULT_OK;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -22,8 +28,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -45,8 +54,12 @@ import com.example.dacn.R;
 import com.example.dacn.adapters.CartListAdapter;
 import com.example.dacn.databinding.FragmentPaymentBinding;
 import com.example.dacn.models.CartItem;
+import com.example.dacn.models.Food;
 import com.example.dacn.viewmodels.ShopViewModel;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -59,6 +72,8 @@ public class PaymentFragment extends Fragment implements CartListAdapter.CartInt
     private static final int REQUEST_CODE = 1234;
     final String API_GET_TOKEN = "http://10.0.2.2:8080/braintree/main.php";
     final String API_CHECK_OUT = "http://10.0.2.2:8080/braintree/checkout.php";
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener;
 
     String token, amount;
     HashMap<String, String> paramsHash;
@@ -76,7 +91,6 @@ public class PaymentFragment extends Fragment implements CartListAdapter.CartInt
         fragmentPaymentBinding = FragmentPaymentBinding.inflate(inflater, container, false);
         return fragmentPaymentBinding.getRoot();
     }
-
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -106,11 +120,57 @@ public class PaymentFragment extends Fragment implements CartListAdapter.CartInt
         RadioButton rb2 = (RadioButton) getView().findViewById(R.id.radioButton2);
         paymentTotalTextView=(TextView) getView().findViewById(R.id.paymentTotalTextView);
         payNowButton=(Button) getView().findViewById(R.id.payNowButton);
+        EditText edtTime = (EditText) getView().findViewById(R.id.edtTime);
+        EditText edtDate = (EditText) getView().findViewById(R.id.edtDate);
+        edtDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                DialogFragment picker = new DatePickerFragment();
+//                picker.show(getFragmentManager(), "datePicker");
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(getActivity(), android.R.style.Theme_DeviceDefault_Light_Dialog, mDateSetListener, year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.LTGRAY));
+                dialog.show();
+            }
+        });
+        mDateSetListener = new DatePickerDialog.OnDateSetListener(){
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                month = month + 1;
+//                Log.d("date", "onDateSet: " + month + "/" + day + "/" + year );
+                    edtDate.setText(day + "/" + month + "/" + year);
+            }
+        };
+
+        edtTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int hour = cal.get(Calendar.HOUR_OF_DAY);
+                int min = cal.get(Calendar.MINUTE);
+
+                TimePickerDialog dialog = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hour, int min) {
+//                        Log.d("time", "onDateSet: " + hour + "/" + min);
+                        edtTime.setText(hour + ":" + min);
+                    }
+                }, hour, min, true);
+                dialog.show();
+            }
+        });
+
 
         fragmentPaymentBinding.payNowButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (rb1.isChecked()) {
+//                    Log.d("TAG", "onClick: " + paymentTotalTextView.getText().toString());
+//                    Log.d("TAG", "onClick: " + );
                     shopViewModel.resetCart();
                     navController.navigate(R.id.action_paymentFragment_to_orderFragment);
                 } else if (rb2.isChecked()) {
